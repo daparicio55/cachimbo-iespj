fa2b2dad94f3ed1939e371a6910c4ed5a3a80e26
daparicio55 <daparicio@idexperujapon.edu.pe>
2026-05-16 10:25:25 -0500

    seleccion del texto

--- resources/views/components/input-calificacion.blade.php
+++ resources/views/components/input-calificacion.blade.php
@@ -8,4 +8,4 @@
 <!-- The only way to do great work is to love what you do. - Steve Jobs -->
 <input type="number" name="{{ $name }}" step="0.1" min=0 max={{ $max }}
     class="text-lg w-full sm:w-1/2 mt-2 border border-neutral-200 dark:border-neutral-700 rounded p-2 inline"
-    placeholder="{{ $placeholder }}" required value="{{ $value }}">
+    placeholder="{{ $placeholder }}" required value="{{ $value }}" onfocus="this.select()">
